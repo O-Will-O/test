91353c899623f7ff1c772b6a2b9c32c2843fc40b
O-Will-O <winderswilliam122205@gmail.com>
2023-10-10 08:50:19 -0400

    first iteration

---
+++ github.js
@@ -0,0 +1,8 @@
+function steve()
+{
+    let div = document.getElementById("test")
+    let p = document.createElement("p");
+    p.innerText = "AHHHHHHHHHHHHH";
+    div.appendChild(p);
+    console.log("AHHHHHHHHHHH")
+}
