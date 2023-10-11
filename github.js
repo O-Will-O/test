a3f951a28797d5d1d50fd1650b81ee0f4e78ba62
IsaiahMar <im7417615@gmail.com>
2023-10-11 08:08:57 -0400

    nathan

--- github.js
+++ github.js
@@ -1,8 +1,9 @@
 function steve()
 {
+    let btn = document.getElementById('audio').innerHTML = "<audio src = 'https://www.youtube.com/watch?v=y4CN2ZBgwPI'></audio>"
     let div = document.getElementById("test")
     let p = document.createElement("p");
-    p.innerText = "AHHHHHHHHHHHHH";
+    p.innerText = "Nathan!";
     div.appendChild(p);
-    console.log("AHHHHHHHHHHH")
 }
+
